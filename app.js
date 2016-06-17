75333f955811611a8c58022cc585ca8f6ac60f11
Julia Schultz <julia.schultz91@gmail.com>
2016-06-17 08:27:03 -0400

    api lab for submission

--- app.js
+++ app.js
@@ -1,49 +1,72 @@
 $(document).ready(function(){
 
+  $('input').keypress(function (e) {
+  if (e.keycode === 13) {
+    $('button').submit();
+  }
+});
+
   var url = "https://api.spotify.com/v1/search"
 
-  // var client_id = 'dbcc4e0718c64438976ff9bdc4af4101'; // Your client id
-  // var client_secret = '12b221f2b3d84d008f1f06d9f25e079a'; // Your secret
-
-
-
-
   $("button").on("click", function(){
-    var artistSearch = $("#keyword").val();
-    console.log(artistSearch);
+
+    $('#relatedArtists img').remove();
+    var keyword = $("#keyword").val();
+    console.log(keyword);
     $.ajax({
       url: url,
       type: "GET",
       data: {
-        q: artistSearch,
-        type: 'album'
+        q: keyword,
+        type: 'artist'
       },
       dataType: "json"
     }).done(function(response){
-      console.log("omg U SO SMART?!?!?", response)
+      test=response;
+      console.log("omg u so SMRT?!", response);
+      var artistSearch = response.artists.items[0].id;
+      var artistChosen = response.artists.items[0].name;
 
+      $("h1").html("so you like "+artistChosen+"? check out these bands:")
 
-
-
-
+      relatedArtists(artistSearch);
     }).fail(function(response){
-      console.log("nobody who hasn't failed succeeds", response)
+      console.log("failure is the tuition you pay for success", response)
     })
+
+
+    var relatedArtists = function (artistSearch){
+      $.ajax({
+        url: "https://api.spotify.com/v1/artists/"+artistSearch+"/related-artists",
+        type: "GET",
+        data: {
+          type: 'related-artists'
+        },
+        dataType: "json"
+      }).done(function(response){
+        console.log("RELATED ARTISTS SUCCESS", response);
+        for (var i=0; i<response.artists.length; i++){
+          var imgUrl = response.artists[i].images[0].url;
+          var artistName = response.artists[i].name;
+          var artistPageUrl = response.artists[i].external_urls.spotify
+
+          var link=$('<a href="'+artistPageUrl+'"></a>')
+
+          var img = $("<img/>");
+          img.attr("src", imgUrl)
+          img.attr("alt", artistName)
+          link.append(img);
+          $("#relatedArtists").append(link);
+
+          var artistHeading = '<h2><span>'+artistName+'</span></h2>'
+          $("#relatedArtists img").append(artistHeading);
+        }
+
+
+      }).fail(function(response){
+        console.log("failure is the tuition you pay for success pt II", response)
+      })
+    }
   });
 
-
-
-
-
-
 });
-
-
-
-
-
-
-
-
-//matches to artist id
-//id is included in the search url for GET
